f44e63816ebf231cccc5eab19a155f269b718fa7
M Miller <melissa.miller@pantheon.io>
2023-07-07 11:54:38 -0700

    begin experimenting with PDS
    Start to untangle local css

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -1,24 +1,38 @@
 import { graphql, StaticQuery, Link } from "gatsby"
 import React from "react"
+import { Button, Panel } from "@pantheon-systems/pds-toolkit-react"
 import MarketoForm from "../../components/marketoForm"
-import './style.css';
+import "./style.css"
 
 const Footer = ({ data }) => {
   const pantheonLogo = data.allFile.edges.find(
-    file => file.node.publicURL.indexOf("pantheon-logo-symbol") > -1
+    (file) => file.node.publicURL.indexOf("pantheon-logo-symbol") > -1
   )
 
   const CCLogo = data.allFile.edges.find(
-    file => file.node.publicURL.indexOf("CC-BY-SA_icon") > -1
+    (file) => file.node.publicURL.indexOf("CC-BY-SA_icon") > -1
   )
 
   return (
     <div>
+      <Panel variant="sunken">
+        <h3>Sample Panel Data</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae et
+          leo duis ut diam quam nulla porttitor massa. Facilisi morbi tempus
+          iaculis urna id.
+        </p>
+        <Button label="Test Button" />
+      </Panel>
       <div className="pio-docs-footer hidden-sf" id="footer">
         <div id="office-hours" className="office-hours">
           <p>Got questions? We've got answers!</p>
           <p>
-            <a href="https://pantheon.io/learn-pantheon?docs" className="demo-cta cta docs-cta">
+            <a
+              href="https://pantheon.io/learn-pantheon?docs"
+              className="demo-cta cta docs-cta"
+            >
               Learn Pantheon
             </a>{" "}
             <a
@@ -48,19 +62,13 @@ const Footer = ({ data }) => {
                       <h4 className="block__title">Topics</h4>
                       <ul className="menu">
                         <li className="first leaf menu-mlid-1147">
-                          <Link to="/get-started/">
-                            Get Started
-                          </Link>
+                          <Link to="/get-started/">Get Started</Link>
                         </li>
                         <li className="leaf menu-mlid-1148">
-                          <Link to="/develop/">
-                            Develop
-                          </Link>
+                          <Link to="/develop/">Develop</Link>
                         </li>
                         <li className="leaf menu-mlid-1149">
-                          <Link to="/go-live/">
-                            Go Live
-                          </Link>
+                          <Link to="/go-live/">Go Live</Link>
                         </li>
                         <li className="leaf menu-mlid-1162">
                           <Link to="/platform/">
@@ -72,14 +80,10 @@ const Footer = ({ data }) => {
                     <div className="col-xs-6 col-sm-3 col-no-title">
                       <ul className="menu">
                         <li className="first leaf menu-mlid-1147">
-                          <Link to="/automate/">
-                            Automate & Integrate
-                          </Link>
+                          <Link to="/automate/">Automate & Integrate</Link>
                         </li>
                         <li className="leaf menu-mlid-1148">
-                          <Link to="/performance/">
-                            Optimize Performance
-                          </Link>
+                          <Link to="/performance/">Optimize Performance</Link>
                         </li>
                         <li className="leaf menu-mlid-1149">
                           <Link to="/manage/">
@@ -87,9 +91,7 @@ const Footer = ({ data }) => {
                           </Link>
                         </li>
                         <li className="leaf menu-mlid-1162">
-                          <Link to="/troubleshoot/">
-                            Troubleshoot
-                          </Link>
+                          <Link to="/troubleshoot/">Troubleshoot</Link>
                         </li>
                       </ul>
                     </div>
@@ -101,10 +103,18 @@ const Footer = ({ data }) => {
                       <h4 className="block__title">Help</h4>
                       <ul className="menu">
                         <li className="first leaf menu-mlid-1151">
-                          <a href="https://pantheon.io/contact-us" className="cta docs-cta">Contact</a>
+                          <a
+                            href="https://pantheon.io/contact-us"
+                            className="cta docs-cta"
+                          >
+                            Contact
+                          </a>
                         </li>
                         <li className="leaf menu-mlid-1152">
-                          <a href="https://directory.pantheon.io/agencies?docs" className="cta docs-cta">
+                          <a
+                            href="https://directory.pantheon.io/agencies?docs"
+                            className="cta docs-cta"
+                          >
                             Hire an Agency
                           </a>
                         </li>
@@ -112,9 +122,7 @@ const Footer = ({ data }) => {
                           <a href="https://status.pantheon.io/">Status</a>
                         </li>
                         <li className="last leaf menu-mlid-1154">
-                          <Link to="/guides/support/">
-                            Support
-                          </Link>
+                          <Link to="/guides/support/">Support</Link>
                         </li>
                       </ul>
                     </div>
@@ -122,14 +130,10 @@ const Footer = ({ data }) => {
                       <h4 className="block__title">Community</h4>
                       <ul className="menu">
                         <li className="first leaf menu-mlid-1142">
-                          <Link to="/code-of-conduct/">
-                            Code of Conduct
-                          </Link>
+                          <Link to="/code-of-conduct/">Code of Conduct</Link>
                         </li>
                         <li className="leaf menu-mlid-1188">
-                          <Link to="/contributors/">
-                            Contributors
-                          </Link>
+                          <Link to="/contributors/">Contributors</Link>
                         </li>
                         <li className="leaf menu-mlid-1143">
                           <Link to="/pantheon-community/">
@@ -137,9 +141,7 @@ const Footer = ({ data }) => {
                           </Link>
                         </li>
                         <li className="last leaf menu-mlid-1145">
-                          <Link to="/contribute/">
-                            Learn to Contribute
-                          </Link>
+                          <Link to="/contribute/">Learn to Contribute</Link>
                         </li>
                       </ul>
                     </div>
@@ -168,8 +170,8 @@ const Footer = ({ data }) => {
                     <div className="col-md-12">
                       <div className="copyright">
                         © 2014-{new Date().getFullYear()} Pantheon{" "}
-                        <span>|</span> 717 California Street, San Francisco,
-                        CA <span className="responsive-pipe">|</span>{" "}
+                        <span>|</span> 717 California Street, San Francisco, CA{" "}
+                        <span className="responsive-pipe">|</span>{" "}
                         <a
                           className="new-line"
                           href="https://pantheon.io/privacy"
@@ -202,7 +204,12 @@ const Footer = ({ data }) => {
                               href="https://www.facebook.com/getpantheon"
                               title="Follow Pantheon on Facebook"
                             >
-                              <i className="fa fa-facebook" aria-label="Facebook" role="img" focusable="false"/>
+                              <i
+                                className="fa fa-facebook"
+                                aria-label="Facebook"
+                                role="img"
+                                focusable="false"
+                              />
                               <span className="sr-only">Facebook</span>
                             </a>
                           </li>
@@ -212,7 +219,12 @@ const Footer = ({ data }) => {
                               href="https://twitter.com/getpantheon"
                               title="Pantheon on Twitter"
                             >
-                              <i className="fa fa-twitter" aria-label="Twitter" role="img" focusable="false" />
+                              <i
+                                className="fa fa-twitter"
+                                aria-label="Twitter"
+                                role="img"
+                                focusable="false"
+                              />
                               <span className="sr-only">Twitter</span>
                             </a>
                           </li>
@@ -222,7 +234,12 @@ const Footer = ({ data }) => {
                               href="https://www.linkedin.com/company/pantheon-systems-inc"
                               title="Pantheon on LinkedIn"
                             >
-                              <i className="fa fa-linkedin" aria-label="LinkedIn" role="img" focusable="false" />
+                              <i
+                                className="fa fa-linkedin"
+                                aria-label="LinkedIn"
+                                role="img"
+                                focusable="false"
+                              />
                               <span className="sr-only">LinkedIn</span>
                             </a>
                           </li>
@@ -232,7 +249,12 @@ const Footer = ({ data }) => {
                               href="https://www.youtube.com/user/GetPantheon"
                               title="Pantheon on YouTube"
                             >
-                              <i className="fa fa-youtube-play"  aria-label="YouTube" role="img" focusable="false" />
+                              <i
+                                className="fa fa-youtube-play"
+                                aria-label="YouTube"
+                                role="img"
+                                focusable="false"
+                              />
                               <span className="sr-only">YouTube</span>
                             </a>
                           </li>
@@ -254,9 +276,8 @@ const Footer = ({ data }) => {
                             <h4>INSIGHT. DELIVERED MONTHLY.</h4>
                             <p>
                               Awesome development news, tutorials, and tips.
-                              Plus get 3 free downloads, just for signing up.
-                              If you don't love it, unsubscribe with just a
-                              click.
+                              Plus get 3 free downloads, just for signing up. If
+                              you don't love it, unsubscribe with just a click.
                             </p>
                           </div>
                           <div className="col-lg-12 col-sm-6 market-subscribe-footer">
@@ -285,7 +306,7 @@ const Footer = ({ data }) => {
   )
 }
 
-export default props => (
+export default (props) => (
   <StaticQuery
     query={graphql`
       {
@@ -305,6 +326,6 @@ export default props => (
         }
       }
     `}
-    render={data => <Footer data={data} {...props} />}
+    render={(data) => <Footer data={data} {...props} />}
   />
 )
